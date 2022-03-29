2ad2821c1147f61668678aff148549a31bde8157
Luis Espinoza <luis.espinoza.nav@live.com>
2022-03-28 21:05:49 -0300

    fix header position, fix title position

--- src/components/Header/HeaderView.js
+++ src/components/Header/HeaderView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import "./Header.css";
 import Hamburger from "hamburger-react";
