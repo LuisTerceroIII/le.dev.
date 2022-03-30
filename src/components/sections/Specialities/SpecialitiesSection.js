f488e3a247634e5887f4b96b8e7dd07b887bf8cb
Luis Espinoza <luis.espinoza.nav@live.com>
2022-03-30 12:47:11 -0300

    add one speciality

--- src/components/sections/Specialities/SpecialitiesSection.js
+++ src/components/sections/Specialities/SpecialitiesSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SpecialitiesView from "./SpecialitiesView";
 
 const SpecialitiesSection = () => {
-    const specialities = ['front end', 'back end', 'full stack'];
+    const specialities = ['front end', 'back end', 'full stack','Java Dev'];
     return (
        <SpecialitiesView specialities={specialities}/>
     );
