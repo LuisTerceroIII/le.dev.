719837b3a9acf5236071e86125963f2eb719bb34
Luis Espinoza <luis.espinoza.nav@live.com>
2023-03-28 00:43:55 -0300

    update education and about me section

--- src/components/sections/AboutMe/AboutMeView.js
+++ src/components/sections/AboutMe/AboutMeView.js
@@ -6,16 +6,15 @@ import './AboutMe.css';
 const AboutMeView = ({about}) => {
 
     return (
-
-            <section className={"about-me-section-container"}>
-                <h2 className={'about-me-title'}>About me</h2>
-                <article className={'about-me-content-container'}>
-                    <Tilt options={{max: 35, perspective: 1000}} className={"avatar-image-tilt"}>
-                        <img src={"./sun3.svg"} alt={"avatar-luis-espinoza"} className={'avatar-image-about-me'}/>
-                    </Tilt>
-                    <p className={'about-paragraph-right'}>{about}</p>
-                </article>
-            </section>
+        <section className={"about-me-section-container"}>
+            <h2 className={'about-me-title'}>About me</h2>
+            <article className={'about-me-content-container'}>
+                <Tilt options={{max: 35, perspective: 1000}} className={"avatar-image-tilt"}>
+                    <img src={"./sun3.svg"} alt={"avatar-luis-espinoza"} className={'avatar-image-about-me'}/>
+                </Tilt>
+                <p className={'about-paragraph-right'}>{about}</p>
+            </article>
+        </section>
     );
 };
 
