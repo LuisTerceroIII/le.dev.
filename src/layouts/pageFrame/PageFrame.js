68bd3198891d5c75d0255586314c0acb92289542
Luis Espinoza <luis.espinoza.nav@live.com>
2023-04-06 19:58:54 -0300

    try fux footer position

--- src/layouts/pageFrame/PageFrame.js
+++ src/layouts/pageFrame/PageFrame.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+const styles = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+}
+
 const PageFrame = ({children}) => {
     return (
-        <main>
+        <main style={styles}>
             <Header />
             {children}
             <Footer/>
