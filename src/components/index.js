b24f304f2433a8e4b09daeeb2f9de9f9c54050af
Luis Espinoza <luis.espinoza.nav@live.com>
2023-08-20 03:40:29 -0300

    divide text compo

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export * from "./organisms/LuisEspinozaComponentText/LuisEspinozaComponent"
---
+++ src/components/organisms/LuisEspinozaComponentText/LuisEspinozaComponent.jsx
@@ -0,0 +1,235 @@
+import React from "react";
+import AnimatedText from "react-animated-text-content";
+import "./LuisEspinozaComponentStyles.css"
+export const LuisEspinozaComponent = () => {
+  return (
+    <section
+      style={{
+        marginTop: 200,
+        display: "flex",
+        flexDirection: "column",
+        padding: "0 5%",
+      }}
+    >
+      <AnimatedText
+        className={"home-word purple-word"}
+        animationType={"float"}
+        type={"words"}
+        tag={"p"}
+        duration={0.6}
+      >
+        {`&#60;LuisEspinoza`}
+      </AnimatedText>
+      <p
+        className="home-word"
+        style={{ display: "flex", flexDirection: "row", marginLeft: "17%" }}
+      >
+        <AnimatedText
+          className="home-word golden-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"type"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word orange-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"="}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"{"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word white-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"SOFTWARE"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word orange-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"."}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word white-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"DEV"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.9}
+        >
+          {"}"}
+        </AnimatedText>
+      </p>
+      <div style={{ display: "flex", flexDirection: "row", marginLeft: "17%" }}>
+        <AnimatedText
+          className="home-word golden-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"specialty"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word orange-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"="}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"{"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word golden-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.1}
+        >
+          {"["}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word green-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.3}
+        >
+          {'"React"'}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word orange-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.3}
+        >
+          {","}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word green-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.5}
+          style={{ whiteSpace: "nowrap" }}
+        >
+          {'"React Native"'}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word golden-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.5}
+        >
+          {"]"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.6}
+        >
+          {"}"}
+        </AnimatedText>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "row", marginLeft: "17%" }}>
+        <AnimatedText
+          className="home-word golden-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"experience"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word orange-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"="}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1}
+        >
+          {"{"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word white-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={1.5}
+        >
+          {"2"}
+        </AnimatedText>
+        <AnimatedText
+          className="home-word purple-word"
+          animationType={"float"}
+          type={"words"}
+          tag={"p"}
+          duration={0.6}
+        >
+          {"}"}
+        </AnimatedText>
+      </div>
+
+      <AnimatedText
+        className="home-word purple-word"
+        animationType={"float"}
+        type={"words"}
+        tag={"p"}
+        duration={0.6}
+      >
+        {"/>"}
+      </AnimatedText>
+    </section>
+  );
+};
