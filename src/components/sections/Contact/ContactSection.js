609c6bebb47daf69143fc9729cc4d8fe33f70ebc
Luis Espinoza <luis.espinoza.nav@live.com>
2023-08-22 00:43:44 -0300

    update service email

--- src/components/sections/Contact/ContactSection.js
+++ src/components/sections/Contact/ContactSection.js
@@ -9,7 +9,7 @@ const ContactSection = () => {
   const sendEmail = (data, e) => {
     setSendingEmail(true);
     emailjs
-      .sendForm("service_u2gx8mk", "le.dev.", form.current, "YzVbZxgxspSW6djNK")
+      .sendForm("service_qqkc6dj", "le.dev.", form.current, "YzVbZxgxspSW6djNK")
       .then(
         (result) => {
           setSendingEmail(false);
