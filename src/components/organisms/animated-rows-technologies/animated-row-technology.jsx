056a4a3f937658a40d0879ba7c45c87c4fc00cf4
Luis Espinoza <luis.espinoza.nav@live.com>
2023-08-22 00:36:31 -0300

    disabled lint on memo

--- src/components/organisms/animated-rows-technologies/animated-row-technology.jsx
+++ src/components/organisms/animated-rows-technologies/animated-row-technology.jsx
@@ -2,13 +2,14 @@ import React, { useMemo } from 'react'
 import "./animated-rows-technologies-styles.css"
 
 export const AnimatedRowTechnology = ({techs, left}) => {
-
+    //eslint-disable-next-line
     const techsImages = useMemo(() => {
         return techs.map(techLocalUriImage => {
             const fileName = techLocalUriImage.split("/").at(-1)
             return <img src={techLocalUriImage} className='tech-image' alt={fileName} key={fileName}/>
         })
-    }, [false])
+    //eslint-disable-next-line    
+    }, [])
     
     return (
         <section className={`technologies-animated-row ${left ? "start-left" : "start-right"}`}>
